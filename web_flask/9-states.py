358f79a5a52ecaa22dab63cddad54607fde3ffe3
besthor <igbebestor7@gmail.com>
2023-04-23 21:42:26 -0700

    States and state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,9 +4,8 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
-from models.state import State
+from models import *
 from models import storage
-
 app = Flask(__name__)
 
 
@@ -14,7 +13,7 @@ app = Flask(__name__)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
     """display the states and cities listed in alphabetical order"""
-    states = storage.all(State)
+    states = storage.all("State")
     if state_id is not None:
         state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
